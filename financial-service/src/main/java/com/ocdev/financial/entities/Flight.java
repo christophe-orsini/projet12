6545d545e72966ce425c47efd8aaec7b1fd6bde5
Christophe <christophe.orsini@gmail.com>
2021-05-21 08:57:08 +0200

    Création entités

---
+++ financial-service/src/main/java/com/ocdev/financial/entities/Flight.java
@@ -0,0 +1,157 @@
+package com.ocdev.financial.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+/**
+ * Flight représente la classe d'un vol.
+ * @author PC_ASUS
+ *
+ */
+@Entity
+@Hidden
+public class Flight implements Serializable
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable=false)
+	private long memberId;
+	@Column(nullable=false, length=100)
+	private String aircraft;
+	@Column(nullable=false, length=100)
+	private String lineItem;
+	@Column(nullable=false)
+	private Date flightDate;
+	@Column(nullable=false)
+	private double flightHours;
+	@Column(nullable=false)
+	private double amount;
+	private Date paymentDate;
+	private double payment;
+	private boolean closed;
+	
+	public Flight()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Flight(long memberId, String aircraft, String lineItem, Date flightDate, double flightHours)
+	{
+		super();
+		this.memberId = memberId;
+		this.aircraft= aircraft;
+		this.lineItem = lineItem;
+		this.flightDate = flightDate;
+		this.flightHours = flightHours;
+	}
+
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public long getMemberId()
+	{
+		return memberId;
+	}
+
+	public void setMemberId(long memberId)
+	{
+		this.memberId = memberId;
+	}
+
+	public String getAircraft()
+	{
+		return aircraft;
+	}
+
+	public void setAircraft(String aircraft)
+	{
+		this.aircraft = aircraft;
+	}
+
+	public String getLineItem()
+	{
+		return lineItem;
+	}
+
+	public void setLineItem(String lineItem)
+	{
+		this.lineItem = lineItem;
+	}
+
+	public Date getFlightDate()
+	{
+		return flightDate;
+	}
+
+	public void setFlightDate(Date flightDate)
+	{
+		this.flightDate = flightDate;
+	}
+
+	public double getFlightHours()
+	{
+		return flightHours;
+	}
+
+	public void setFlightHours(double flightHours)
+	{
+		this.flightHours = flightHours;
+	}
+
+	public double getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(double amount)
+	{
+		this.amount = amount;
+	}
+
+	public Date getPaymentDate()
+	{
+		return paymentDate;
+	}
+
+	public void setPaymentDate(Date paymentDate)
+	{
+		this.paymentDate = paymentDate;
+	}
+
+	public double getPayment()
+	{
+		return payment;
+	}
+
+	public void setPayment(double payment)
+	{
+		this.payment = payment;
+	}
+
+	public boolean isClosed()
+	{
+		return closed;
+	}
+
+	public void setClosed(boolean closed)
+	{
+		this.closed = closed;
+	}
+}
---
+++ financial-service/src/main/java/com/ocdev/financial/entities/Membership.java
@@ -0,0 +1,98 @@
+package com.ocdev.financial.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+/**
+ * Finance représente la classe d'une adhésion à l'aéroclub.
+ * @author PC_ASUS
+ *
+ */
+@Entity
+@Hidden
+public class Membership implements Serializable
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable=false)
+	private long memberId;
+	@Column(nullable=false)
+	private Date paymentDate;
+	@Column(nullable=false)
+	private double amount;
+	@Column(nullable=false)
+	private Date validityDate;
+	
+	public Membership()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Membership(long memberId, Date paymentDate, double amount)
+	{
+		super();
+		this.memberId = memberId;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+	}
+
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public long getMemberId()
+	{
+		return memberId;
+	}
+
+	public void setMemberId(long memberId)
+	{
+		this.memberId = memberId;
+	}
+
+	public Date getPaymentDate()
+	{
+		return paymentDate;
+	}
+
+	public void setPaymentDate(Date paymentDate)
+	{
+		this.paymentDate = paymentDate;
+	}
+
+	public double getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(double amount)
+	{
+		this.amount = amount;
+	}
+
+	public Date getValidityDate()
+	{
+		return validityDate;
+	}
+
+	public void setValidityDate(Date validityDate)
+	{
+		this.validityDate = validityDate;
+	}
+}
