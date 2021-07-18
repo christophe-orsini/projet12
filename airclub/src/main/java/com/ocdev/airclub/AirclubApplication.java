419a8f3c2942f2994cd9aa9a2a93f90dd90b249b
Christophe <christophe.orsini@gmail.com>
2021-07-18 10:07:10 +0200

    Suppression dependances avec Config et Eureka

--- airclub/src/main/java/com/ocdev/airclub/AirclubApplication.java
+++ airclub/src/main/java/com/ocdev/airclub/AirclubApplication.java
@@ -3,9 +3,7 @@ package com.ocdev.airclub;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class AirclubApplication implements CommandLineRunner 
 {
