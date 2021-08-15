ef540663bf9a465b1f6337ac1fc3311e9c60cc7f
Christophe <christophe.orsini@gmail.com>
2021-08-15 17:17:11 +0200

    Ajout du endpoint pour obtenir la liste des vols payés

--- financial-service/src/main/java/com/ocdev/financial/controllers/FinancialController.java
+++ financial-service/src/main/java/com/ocdev/financial/controllers/FinancialController.java
@@ -63,7 +63,21 @@ public class FinancialController
 			@ApiParam(value = "Id du membre", required = true, example = "2ffe-5a55") @PathVariable final String memberId)
 			throws EntityNotFoundException
 	{
-		Collection<Flight> flights = _financialService.getAllFlights(memberId);
+		Collection<Flight> flights = _financialService.getAllFlights(memberId, false);
+		return new ResponseEntity<Collection<Flight>>(flights, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Obtenir les vols d'un membre", notes = "Obtenir la liste des vols payés par un membre")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "La liste est retournée dans le corps de la réponse"),
+			@ApiResponse(code = 401, message = "Authentification requise")
+			})
+	@GetMapping(value = "/flights/{memberId}/paid", produces = "application/json")
+	public ResponseEntity<Collection<Flight>> getPaidFlights(
+			@ApiParam(value = "Id du membre", required = true, example = "2ffe-5a55") @PathVariable final String memberId)
+			throws EntityNotFoundException
+	{
+		Collection<Flight> flights = _financialService.getAllFlights(memberId, true);
 		return new ResponseEntity<Collection<Flight>>(flights, HttpStatus.OK);
 	}
 	
