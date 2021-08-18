38f6b9a4cc3b9bd138010bbf22c568fc429e7ab8
Christophe <christophe.orsini@gmail.com>
2021-08-18 08:47:07 +0200

    Création envoi de l'email dans Finance

---
+++ financial-service/src/main/java/com/ocdev/financial/services/EmailService.java
@@ -0,0 +1,16 @@
+package com.ocdev.financial.services;
+
+import javax.mail.MessagingException;
+
+public interface EmailService
+{
+	/**
+	 * Envoi d'un email au format HTML
+	 * @param to Le destinataire
+	 * @param content Le contenu de l'email
+	 * @param subject Le sujet de l'email
+	 * @param from L'expediteur
+	 * @throws MessagingException
+	 */
+	public void envoiEmailHtml(String to, String content, String subject, String from) throws MessagingException;
+}
